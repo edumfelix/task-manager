b902e59a690c006663d4ac02a158e8b863d47e81
Eduardo Felix <eduardofelixdev@gmail.com>
2023-10-22 11:43:55 -0300

    feat: criando controller TasksController e o primeiro método (Get All)

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly Context _context;
+        public TasksController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Task>>> GetAllAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+    }
+}
